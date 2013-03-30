0d0a404e1c11cf4b4b045aaaa72db5b56f421102
mickail voyiatzis <mickailvoyiatzis@gmail.com>
2013-03-30 12:43:36 +0100

    J'ai ajoute des choses sur l'interface pour le client concernant sa commande

--- project/src/myxmlproject/Main.java
+++ project/src/myxmlproject/Main.java
@@ -24,8 +24,6 @@ public class Main {
 
 	private static ArrayList<Company> companies = new ArrayList<Company>();
 
-	
-
 	public static void main(String[] args) {
 
 		// Initializing
@@ -75,7 +73,46 @@ public class Main {
 								+ (nbDollars + nbEuros)
 								+ " check have been generated in your customer directory:");
 				System.out.println("--> " + c.getDirectory());
-
+				// Choisir entreprise-produit
+				System.out
+						.println("Please choose the company you wish from the list below: ");
+				for (int i = 0; i <= companies.size(); i++) {
+					System.out.println(i + ") " + "Type -" + i + "- for "
+							+ companies.get(i).getName());
+				}
+				int idCompany = Integer.parseInt(stdinp.readLine());
+				int selection = 3;
+				while (selection == 3) {
+					System.out
+							.println("Plesae choose a product from the list below: ");
+					for (int i = 0; i <= companies.get(idCompany)
+							.getProductList().size(); i++) {
+						System.out.println(i
+								+ ") "
+								+ "Type -"
+								+ i
+								+ "- if you want to buy"
+								+ companies.get(idCompany).getProductList()
+										.get(i).getName());
+					}
+					Product product = companies.get(idCompany).getProductList()
+							.get(Integer.parseInt(stdinp.readLine()));
+					System.out.println("The price of the product selected is "
+							+ product.getUnitPrice() + " "
+							+ companies.get(idCompany).getCurrency());
+					System.out
+							.println("Select the quantity that you wish to buy: ");
+					int quantity = Integer.parseInt(stdinp.readLine());
+					float totalPrice = quantity * product.getUnitPrice();
+					System.out.println("The total price of your selection is "
+							+ totalPrice + " "
+							+ companies.get(idCompany).getCurrency());
+					System.out
+							.println("If you want to validate your command, please type 1. "
+									+ "If you want to cancel your command, please type 2."
+									+ "If you want to buy another product, please type 3");
+					selection = Integer.parseInt(stdinp.readLine());
+				}
 			} else if (input.equals("No")) {
 				System.out.println("Customer already exists");
 			}
@@ -92,7 +129,7 @@ public class Main {
 		// 7. Enregister le client chez le GdC
 		// Attention gestion des clients existants avec login
 	}
-	
+
 	private static ArrayList<Company> InitialiseCompanies() {
 		// products
 		Product burger = new Product("burger", 50, 4f);
@@ -124,20 +161,20 @@ public class Main {
 		productTA.add(travel1);
 		productTA.add(travel2);
 		productTA.add(travel3);
-		
-		//initialise Companies
-		Company McDonalds = new Company(1, "McDonalds", 0, 0, "dollars",
+
+		// initialise Companies
+		Company McDonalds = new Company(0, "McDonalds", 0, 0, "dollars",
 				productMc);
-		Company GAP = new Company(2, "GAP", 0, 20, "dollars", productGap);
-		Company TravelAir = new Company(3, "TravelAir", 1, 40, "euros",
+		Company GAP = new Company(1, "GAP", 0, 20, "dollars", productGap);
+		Company TravelAir = new Company(2, "TravelAir", 1, 40, "euros",
 				productTA);
-		Company ZARA = new Company(4, "ZARA", 2, 10, "euros", productZara);
-		
+		Company ZARA = new Company(3, "ZARA", 2, 10, "euros", productZara);
+
 		companies.add(McDonalds);
+		companies.add(GAP);
 		companies.add(TravelAir);
 		companies.add(ZARA);
-		companies.add(GAP);
-		
+
 		return companies;
 	}
 }
